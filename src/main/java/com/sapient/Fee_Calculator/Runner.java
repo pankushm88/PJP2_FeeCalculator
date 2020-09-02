d72d37b37e871958f342b5ba5fa09b391d475163
Pankush Mahajan <42151419+pankushm88@users.noreply.github.com>
2020-09-03 00:14:01 +0530

    FeeCalculator Assignment done successfully

--- src/main/java/com/sapient/Fee_Calculator/Runner.java
+++ src/main/java/com/sapient/Fee_Calculator/Runner.java
@@ -7,6 +7,42 @@ package com.sapient.Fee_Calculator;
  * @author Pankush
  *
  */
+import java.io.IOException;
+import java.util.List;
+
 public class Runner {
 
+public static void main(String[] args)  
+
+	{
+	String inputFile = "input.csv";
+	String outputFile = "output.csv";
+	
+	ReadWriteFile rwf=new ReadWriteFile();
+	List<AttributesPojo> InputList=null;
+	
+	try {
+			InputList = rwf.ReadFile(inputFile);
+		} 
+	catch (IOException e)
+		{
+		
+			e.printStackTrace();
+		}
+	
+	Feecalculation fc=new Feecalculation();
+	List<String> OutputList=fc.calculate(InputList);
+	
+	try
+		{
+			rwf.WriteFile(OutputList,outputFile);
+		}
+	
+	catch (IOException e) 
+		{
+			e.printStackTrace();
+		}
+	
+	}
+	
 }
