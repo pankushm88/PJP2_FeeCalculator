477fac2618efe5f0f0f982a9ddb534b7c8a0fc97
Pankush Mahajan <42151419+pankushm88@users.noreply.github.com>
2020-09-03 00:12:20 +0530

    FeeCalculator Assignment done successfully

--- src/main/java/com/sapient/Fee_Calculator/ReadWriteFile.java
+++ src/main/java/com/sapient/Fee_Calculator/ReadWriteFile.java
@@ -7,6 +7,52 @@ package com.sapient.Fee_Calculator;
  * @author Pankush
  *
  */
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ReadWriteFile {
 
+	public List<AttributesPojo> list;
+	String line="";
+	String[] Fields;
+	
+	
+	public List<AttributesPojo> ReadFile(String inputFile) throws IOException 
+	
+	{
+		list=new ArrayList<AttributesPojo>();
+		BufferedReader bf= new BufferedReader(new FileReader(inputFile));
+		
+		while ((line=bf.readLine())!=null)
+			{
+				Fields=line.split(",");
+
+				list.add(new AttributesPojo(Fields[0],Fields[1],Fields[2],Fields[3],Fields[4],Double.parseDouble(Fields[5]),Fields[6]));
+			}
+		
+		bf.close();
+		
+		return list;
+		
+	}
+	
+	
+	public void WriteFile(List<String> outputList, String outputFile) throws IOException 
+	{
+		FileWriter MyWriter = new FileWriter(outputFile);
+
+		for (String temp : outputList)
+			MyWriter.write(temp + "\n");
+
+			MyWriter.close();
+			
+			System.gc();
+		
+	}
+
 }
+
