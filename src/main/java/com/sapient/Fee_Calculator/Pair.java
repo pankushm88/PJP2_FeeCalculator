f1dd094c11055487716cca0510c3a8e56a886f40
Pankush Mahajan <42151419+pankushm88@users.noreply.github.com>
2020-09-03 00:11:40 +0530

    FeeCalculator Assignment done successfully

--- src/main/java/com/sapient/Fee_Calculator/Pair.java
+++ src/main/java/com/sapient/Fee_Calculator/Pair.java
@@ -7,6 +7,13 @@ package com.sapient.Fee_Calculator;
  * @author Pankush
  *
  */
-public class Pair {
-
+public class Pair 
+{
+AttributesPojo Transaction;
+int Processing_Fee;
+public Pair(AttributesPojo transaction, int processing_Fee) {
+	super();
+	Transaction = transaction;
+	Processing_Fee = processing_Fee;
+}
 }
