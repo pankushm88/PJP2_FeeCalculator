4e60c0d7c5569acf36e463430ee30f6154b84880
Pankush Mahajan <42151419+pankushm88@users.noreply.github.com>
2020-09-03 00:09:47 +0530

    FeeCalculator Assignment done successfully

--- src/main/java/com/sapient/Fee_Calculator/AttributesPojo.java
+++ src/main/java/com/sapient/Fee_Calculator/AttributesPojo.java
@@ -7,6 +7,27 @@ package com.sapient.Fee_Calculator;
  * @author Pankush
  *
  */
-public class AttributesPojo {
+public class AttributesPojo
+{
 
+	String Transaction_ID,Client_ID,Security_Id,Transaction_Type,Priority_Flag,Date;
+	Double Market_value;
+	
+	public AttributesPojo(String transaction_ID, String client_ID, String security_Id, String transaction_Type,
+			 String date, Double market_value,String priority_Flag)
+	{
+		super();
+		Transaction_ID = transaction_ID;
+		Client_ID = client_ID;
+		Security_Id = security_Id;
+		Transaction_Type = transaction_Type;
+		
+		Date = date;
+		
+		Priority_Flag = priority_Flag;
+		Market_value = market_value;
+	}
+
+
+	
 }
